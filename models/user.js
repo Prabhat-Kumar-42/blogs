b6cfd0f0b511489a0980abfe3d1d81150c5aff00
blaze <prabhatkumarverma42@gmail.com>
2024-06-24 21:04:26 +0530

    Updated User Model
    Added Fields:
    1. roles: contains user role.
    2. profile-pic uri: contains profile uri.
    3. salt: contains password salt.
    
    Added pre-save: hash the password b4 saving to db.
    Added static match password: matches password has at the time of login.

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const { randomBytes } = require("crypto");
+const { getHash } = require("../services/hash");
 
 const userSchema = new mongoose.Schema(
   {
@@ -17,9 +19,53 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    role: {
+      type: String,
+      enum: ["USER", "ADMIN"],
+      default: "USER",
+    },
+    profile_pic_url: {
+      type: String,
+      default: "/images/defaults/profile-pic/profile-pic.png",
+    },
+    salt: {
+      type: String,
+    },
   },
   { timestamps: true },
 );
+
+userSchema.pre("save", function (next) {
+  const user = this;
+  if (!user.isModified("password")) {
+    return next();
+  }
+  const salt = randomBytes(16).toString();
+  const hashedPassword = getHash(salt, user.password);
+  this.salt = salt;
+  this.password = hashedPassword;
+  next();
+});
+
+userSchema.static("matchPassword", async function (email, password) {
+  try {
+    const user = await this.findOne({ email: email });
+    if (!user) {
+      return false;
+    }
+    const salt = user.salt;
+    const storedPasswordHash = user.password;
+    const recievedPasswordHash = getHash(salt, password);
+    if (storedPasswordHash != recievedPasswordHash) {
+      return false;
+    }
+    user.password = null;
+    user.salt = null;
+    return user;
+  } catch (error) {
+    return error;
+  }
+});
 
 const USER = mongoose.model("user", userSchema);
 
