80ef268f06d6025e2e6d4ee7d532180dfe664698
blaze <prabhatkumarverma42@gmail.com>
2024-06-26 00:30:01 +0530

    removed extra/not-needed routes

--- routes/staticRoutes.js
+++ routes/staticRoutes.js
@@ -2,21 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 router.route("/").get((req, res) => {
+  const user = req.user;
   return res.status(200).render("home", {
-    user: req.user,
+    user: user,
   });
-});
-
-router.route("/signup").get((req, res) => {
-  return res.status(200).render("signup");
-});
-
-router.route("/login").get((req, res) => {
-  return res.status(200).render("login");
-});
-
-router.route("/createblog").get((req, res) => {
-  return res.status(200).render("blog-form");
 });
 
 module.exports = router;
